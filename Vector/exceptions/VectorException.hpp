288ef484db1cea0c01320fe254dcc714846988ef
Fady-K <popfadykamal151617@gmail.com>
2023-07-01 01:09:35 +0300

    added documentation

--- Vector/exceptions/VectorException.hpp
+++ Vector/exceptions/VectorException.hpp
@@ -3,27 +3,63 @@
 #include <exception>
 #include <string>
 
-class VectorException: public std::exception
+/**
+ * @class VectorException
+ * @brief Exception class for the Vector class.
+ */
+class VectorException : public std::exception
 {
-    /* Instane Attributes */
-protected:
-    std::string m_Msg;
-    
-
-    /* Instance Methods*/
 public:
-    explicit VectorException() noexcept;                                                 // default constructor
-    VectorException(const std::string& t_Msg) noexcept;                                  // parametrized constructor
-    VectorException(const VectorException& otherException) noexcept;                     // default copy constructor
-    VectorException& operator=(const VectorException& otherException) noexcept;          // copy assignment operator
-    ~VectorException() noexcept;                                                         // destructor
+    /**
+     * @brief Default constructor.
+     */
+    VectorException() noexcept;
 
-    /* Setters and Getters */
-    void SetMsg(const std::string& t_Msg) noexcept;
+    /**
+     * @brief Constructor with a custom error message.
+     * @param t_Msg The error message.
+     */
+    explicit VectorException(const std::string& t_Msg) noexcept;
+
+    /**
+     * @brief Copy constructor.
+     * @param otherVector The VectorException object to copy.
+     */
+    VectorException(const VectorException& otherVector) noexcept;
+
+    /**
+     * @brief Assignment operator.
+     * @param otherVector The VectorException object to assign.
+     * @return Reference to the assigned VectorException object.
+     */
+    VectorException& operator=(const VectorException& otherVector) noexcept;
+
+    /**
+     * @brief Destructor.
+     */
+    ~VectorException() override;
+
+    /**
+     * @brief Sets the error message.
+     * @param m_Msg The error message to set.
+     */
+    void SetMsg(const std::string& m_Msg) noexcept;
+
+    /**
+     * @brief Gets the error message.
+     * @return The error message.
+     */
     const std::string& GetMsg() const noexcept;
 
-    // what
-    virtual const char* what() const noexcept override;
+    /**
+     * @brief Returns the error message as a C-style string.
+     * @return The error message as a C-style string.
+     */
+    const char* what() const noexcept override;
+
+private:
+    std::string m_Msg; /**< The error message. */
 };
+
 
 
