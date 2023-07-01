5b458d9529892318a176b2be6058ce18144da017
Fady-K <popfadykamal151617@gmail.com>
2023-07-01 14:18:43 +0300

    documented constructors, assignment operators and Capcity and size operations

--- Vector/include/Vector.hpp
+++ Vector/include/Vector.hpp
@@ -16,6 +16,12 @@
 #include "../Iterator/Iterator.hpp"
 #include "../exceptions/VectorException.hpp"
 
+/**
+ * @class Vector
+ * @brief A dynamic array that provides functionality similar to the standard vector container in stl.
+ * 
+ * @tparam T The type of elements stored in the vector, default type (int)
+ */
 template<class T = int>
 class Vector
 {
@@ -31,23 +37,121 @@ private:
 
 	/* Rule of big 5 and instance methods */
 public:
+	/**
+	 * @brief Default constructor. Constructs an empty vector with zero size and capacity of 1.
+	 * @details Added explicit to add to force the compiler not to ignore with original vector in stl
+	 */
 	explicit Vector();												// default constructor
+
+
+	/**
+	 * @brief Parametrized constructor. Constructs a vector with the specified size and default value.
+	 * @param size The size of the constructed vector.
+	 * @param defaultValue The default value to initialize the elements with, if int = 0.
+	 */
 	Vector(size_t size, const T& defaultValue=0);					// parametrized constructor
+
+	/**
+	 * @brief Constructor that initializes the vector with the elements from given initializer list.
+	 * @param values The initializer list containing the elements to be copied to the vector (deep copy).
+	 */
 	explicit Vector(std::initializer_list<T> values);				// initialize from initializer list
+
+	/**
+	 * @brief Copy constructor. Constructs a vector by deep copying the elements from another vector.
+	 * @details It performs deep copy where a new block of memory is registered and elements get copied from other
+	 * vector to *this using std::copy().
+	 * @param otherVector The vector to be copied.
+	 */
 	Vector(const Vector<T>& otherVector);							// default copy constructor ( deep copy )
+
+	/**
+	 * @brief Move constructor. Constructs a vector by moving the resources from another vector.
+	 * @param otherVector The vector to be moved.
+	 */
 	Vector(Vector<T>&&) noexcept;									// move constructor
+
+	/**
+	 * @brief Copy assignment operator. Assigns the elements from another vector to this vector by deep copying.
+	 * @param otherVector The vector to be copied.
+	 * @return A reference to the updated vector.
+	 */
 	Vector<T>& operator=(const Vector<T>&) noexcept;				// copy assignment operator
+
+	/**
+	 * @brief Move assignment operator. Moves the resources from another vector to this vector.
+	 * @param otherVector The vector to be moved.
+	 * @return A reference to the updated vector.
+	 */
 	Vector<T>& operator=(Vector<T>&&);								// move assignment operator
+
+	/**
+	 * @brief Destructor. Deallocates the dynamically allocated memory of the vector.
+	 * @details In addition to deallocation the memroy, it sets m_Size and Capacity = 0.
+	 */
 	~Vector() noexcept;												// destructor
 
 
 	// Capacity, Size
+
+	/**
+	 * @brief Returns number of elements in the vector.
+	 * @return size_t Number of elements in the vector
+	 */
 	size_t size() const;
+
+	/**
+	 * @brief Returns number of objects this vector can store in the current reserved block of memeory.
+	 * 
+	 * @return size_t Number of objects vector can store within the current reserved block of memory.
+	 */
 	size_t capacity() const;
+
+	/**
+	 * @brief Returns the maximum size this vector can reach in terms of computer resources (Theoratically).
+	 * @details 
+	 * @return size_t Max size this vector can reach in terms of computer reserouces.
+	 */
 	size_t Max_Size() const;
+
+	/**
+	 * @brief Checks if the vector is empty.
+	 * 
+	 * @return true If vector is empty.
+	 * @return false If vector is !empty.
+	 */
 	bool Empty() const;
+
+	/**
+	 * @brief Check whether (current allocated block of memory) is full or not.
+	 * @details (size >= capacity)
+	 * @return true If current allocated block of memory is full.
+	 * @return false If current allocated block of memory isn't full.
+	 */
 	bool Full() const;
+
+	/**
+	 * @brief Reserves memory for a specified capacity.
+	 * @param newCapacity The new capacity to reserve.
+	 * @details If the specified capacity is greater than the current capacity of the vector:
+	 * - Allocate a new memory block with the specified capacity.
+	 * - If there are elements in the vector:
+	 * - Perform deep Copy to the elements from the existing memory block to the new block.
+	 * - Deallocate the old memory block.
+	 * - Update the vector's capacity to the specified capacity.
+	 * - Else (newCapcity <= currentCapacity):
+	 * - Do nothing.
+	 * 
+	 */
 	void Reserve(size_t newCapacity);
+
+	/**
+	 * @brief Reduces the capacity of the vector to match its size (Shrinks vector capacity till it fits the size).
+	 * @details Creates a new memory block with a size equal to the current size of the vector.
+	 * - Copy the elements from the existing memory block to the new block.
+	 * - Deallocate the old memory block.
+	 * - Set the vector's capacity to match its size.
+	 */
 	void Shrink_To_Fit();
 	
 
